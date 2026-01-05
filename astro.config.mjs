2e7c04abb971c8bdb2f795380efec23c93deca44
sGate-Amit <105426175+sGate-Amit@users.noreply.github.com>
2026-01-05 14:44:48 +0530

    Update astro.config.mjs
    code updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
-  site: "https://skyways-air.in/",
-  output: "server", // 🔴 REQUIRED for Cloudflare adapter
+  output: "server",
+  site: "https://your-domain.com",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
+    platformProxy: { enabled: true },
   }),
 });
